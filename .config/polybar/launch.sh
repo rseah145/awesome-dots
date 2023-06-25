81d85aa2c3e6a0b0b4c62b04fb1d91aff01f7c72
b00tl04d <108401257+b00tl04d@users.noreply.github.com>
2023-06-25 14:09:09 +0800

    Update launch.sh

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# From arch wiki polybar, make this script executable "chmod 740 launch.sh"
+# From arch wiki polybar, make this script executable "chmod 710 launch.sh"
 # Terminate already running bar instances
 killall -q polybar
 # If all your bars have ipc enabled, you can also use
