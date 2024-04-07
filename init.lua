3da0bebc637cea322c95179ced13a1a6b146e2ee
rseah145 <108401257+rseah145@users.noreply.github.com>
2024-04-07 19:34:49 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,7 +2,8 @@
 require("config.lazy")
 
 require("toggleterm").setup({
-  open_mapping = [[<c-\>]],
+  open_mapping = [[<c-\>]],  -- ctrl + backslash
+  -- quick direction settings provided are "horizontal", "vertical", and "float"
   direction = "horizontal",
   shade_terminals = true,
 })
